65fffa73036998385f6f33f959ddebaae9a10d66
Omokefe Ovie David <ovie.omokefe@premiumtrustbank.com>
2024-09-16 13:55:48 +0100

    guessing  game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use colored::*; 
 fn main() {
 
     let s1 = String::from("Name of you");
     let lenc = get_string_len(s1);
     println!("the length {}", lenc);
+
+    guessing_game();
     
 }
 
@@ -32,4 +37,41 @@ fn get_string_len(name: String) -> usize {
 
     let lenc = name.len();
     lenc
+}
+
+fn guessing_game(){
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut attempt = 5;
+
+    println!("you have 5 attempt");
+
+    loop{ 
+        let mut guess = String::new();
+
+       if attempt > 0 {
+        attempt = attempt -1; 
+        println!("Enter your guess");
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Did't expect this format");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret_number){
+            Ordering::Greater => println!("{}", "Greater than the value".red()),
+            Ordering::Less => println!("{}", "Lesser than the value".red()),
+            Ordering::Equal => {
+                println!("{}", "Yay, you won!".green());
+                break;
+            }
+        }
+       } else {
+            println!("Out of luck");
+            break;
+       }
+      
+    }
 }
