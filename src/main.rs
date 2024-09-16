6c75c3085ae6dff15411799134e3f14cf707eefe
Omokefe Ovie David <ovie.omokefe@premiumtrustbank.com>
2024-09-16 12:55:28 +0100

    learnig rust from harki...singh

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+
+fn main() {
+
+    let s1 = String::from("Name of you");
+    let lenc = get_string_len(s1);
+    println!("the length {}", lenc);
+    
+}
+
+// Function to compute Fibonacci number
+// fn fibonacci(n: u32) -> u32 {
+//     if n == 0 {
+//         return 0;
+//     } else if n == 1 {
+//         return 1;
+//     }
+
+//     let mut a = 0;
+//     let mut b = 1;
+//     let mut fib = 0;
+
+//     for _ in 2..=n {
+//         fib = a + b;
+//         a = b;
+//         b = fib;
+//     }
+
+//     fib
+// }
+
+fn get_string_len(name: String) -> usize {
+
+    let lenc = name.len();
+    lenc
+}
